702d9fcc69f5224eb06987931b8b81eb8c3abbfc
Pallav <pallobdas07@gmail.com>
2025-10-11 15:46:55 +0530

    Algo updated

--- binary_tree.c
+++ binary_tree.c
@@ -15,12 +15,37 @@ struct Node *createNode(int data){
     return n;
 }
 
+void preorder_traversal(struct Node *root){
+    if (root!=NULL){
+        printf("%d ",root->data);
+        preorder_traversal(root->left);
+        preorder_traversal(root->right);
+    }
+}
+
 int main(){
     struct Node *p=createNode(2);
-    struct Node *p1=createNode(4);
-    struct Node *p2=createNode(6);
+    struct Node *p2=createNode(1);
+    struct Node *p3=createNode(4);
+    struct Node *p4=createNode(11);
+    struct Node *p5=createNode(12);
+    struct Node *p6=createNode(3);
 
-    p->left=p1;
-    p->right=p2;
+    /* This is the tree
+            2
+            /\
+           1  4
+           /\  \
+          11 12 3
+    */
+
+
+    p->left=p2;
+    p->right=p3;
+    p2->left=p4;
+    p2->right=p5;
+    p3->right=p6;
+
+    preorder_traversal(p);
     return 0;
 }
